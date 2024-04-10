4bada10b614768df28e6a883919d51b5f1f5ab95
Geoffrey <brunet.geoffrey@gmail.com>
2024-04-10 17:25:01 +0200

    remplacement nom de variable plus simple

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ function App() {
   const [timeWanted, setTimeWanted] = useState(5)
   const [secondsLeft, setSecondsLeft] = useState(timeWanted)
 
-  const handleChange = (v) => {
-    setTimeWanted(v)
-    setSecondsLeft(v)
+  const handleChange = (inputValue) => {
+    setTimeWanted(inputValue)
+    setSecondsLeft(inputValue)
   }
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setSecondsLeft(v => {
-        if (v <= 1) {
+      setSecondsLeft(inputValue => {
+        if (inputValue <= 1) {
           clearInterval(timer)
           return 0
         }
-        return v - 1
+        return inputValue - 1
 
       })
     }, 1000)
